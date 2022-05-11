8b58f93b4a3c410dbd94919e63503dba3af7f601
Tufan Sadhukhan (MS/ESD-PP2-XC) <tuf1kor@bosch.com>
2022-05-11 19:02:03 +0530

    initial commit

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,20 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::resultStats Statistics::ComputeStatistics(const std::vector<float> &inputVector)
+{
+
+    Statistics::resultStats l_obj;
+
+    l_obj.min = *min_element(inputVector.begin(), inputVector.end());
+    l_obj.max = *max_element(inputVector.begin(), inputVector.end());
+    l_obj.average = std::accumulate(inputVector.begin(), inputVector.end(), 0.0);
+
+    return l_obj;
+}
+
+int main()
+{
+    auto result = Statistics::ComputeStatistics({1.2, 4.5, 6.1, 7.7});
+    std::cout << "min: " << result.min << " max: " << result.max << " avg: " << result.average << std::endl;
+    return 0;
 }
--- stats.h
+++ stats.h
@@ -1,5 +1,21 @@
 #include <vector>
+#include <iostream>
+#include <array>
+#include <cstring>
+#include <numeric>
+#include <bits/stdc++.h>
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+namespace Statistics
+{
+    struct resultStats
+    {
+        float average{0};
+        float max{0};
+        float min{0};
+    };
+    // Stats ComputeStatistics(const std::vector<___> &);
+    // void ComputeStatistics(std::vector<std::array<float, 4>> &inputVector);
+    resultStats ComputeStatistics(const std::vector<float> &inputVector);
+    // void ComputeStatistics(const computedStats &inputVector);
+
 }
