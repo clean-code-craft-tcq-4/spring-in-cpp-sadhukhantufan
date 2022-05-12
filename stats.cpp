2fc653ef2c34da2280168fdd0f690af7a166333f
Tufan Sadhukhan (MS/ESD-PP2-XC) <tuf1kor@bosch.com>
2022-05-12 11:22:02 +0530

    2nd test compiling

--- stats.cpp
+++ stats.cpp
@@ -5,8 +5,11 @@ Statistics::resultStats Statistics::ComputeStatistics(const std::vector<float> &
 
     Statistics::resultStats l_obj;
 
-    l_obj.min = *min_element(inputVector.begin(), inputVector.end());
-    l_obj.max = *max_element(inputVector.begin(), inputVector.end());
+    if (!inputVector.empty())
+    {
+        l_obj.min = *min_element(inputVector.begin(), inputVector.end());
+        l_obj.max = *max_element(inputVector.begin(), inputVector.end());
+    }
     l_obj.average = (std::accumulate(inputVector.begin(), inputVector.end(), 0.0)) / inputVector.size();
 
     return l_obj;
